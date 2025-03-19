2e97ff2840d4da9f84c448e2627e34db3acc421b
harshitasoni15 <harshita.soni.s64@kalvium.community>
2025-03-19 10:54:06 +0530

    connected database with mongodb

--- backend/model/user.js
+++ backend/model/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     unique: true,
     trim: true,
     lowercase: true,
-    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email address']
+    match: [/^\w+([.-]?\w+)@\w+([.-]?\w+)(\.\w{2,3})+$/, 'Please provide a valid email address']
   },
   password: {
     type: String,
